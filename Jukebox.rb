c34afed5a6fb6eff6c9a243562deacd98b1eaadf
Nathan Kwiatkowski <nathankski@gmail.com>
2012-07-14 21:57:22 -0400

    added sound array

--- Jukebox.rb
+++ Jukebox.rb
@@ -8,6 +8,39 @@ class Jukebox
 			Gosu::Sample.new(@window, "./Music/Background/Contrast_background_02.ogg")
 		]
 		
+		@sounds = [
+			Gosu::Sample.new(@windows, "./Music/Effects/concrete_jump_land_01.ogg"),
+			Gosu::Sample.new(@windows, "./Music/Effects/concrete_jump_land_02.ogg"),
+			Gosu::Sample.new(@windows, "./Music/Effects/concrete_jump_land_03.ogg"),
+			Gosu::Sample.new(@windows, "./Music/Effects/concrete_slide_01.ogg"),
+			Gosu::Sample.new(@windows, "./Music/Effects/concrete_slide_02.ogg"),
+			Gosu::Sample.new(@windows, "./Music/Effects/concrete_step_01.ogg"),
+			Gosu::Sample.new(@windows, "./Music/Effects/concrete_step_02.ogg"),
+			Gosu::Sample.new(@windows, "./Music/Effects/concrete_step_03.ogg"),
+			Gosu::Sample.new(@windows, "./Music/Effects/concrete_step_04.ogg"),
+			Gosu::Sample.new(@windows, "./Music/Effects/concrete_step_05.ogg"),
+			Gosu::Sample.new(@windows, "./Music/Effects/grass_step_01.ogg"),
+			Gosu::Sample.new(@windows, "./Music/Effects/grass_step_02.ogg"),
+			Gosu::Sample.new(@windows, "./Music/Effects/grass_step_03.ogg"),
+			Gosu::Sample.new(@windows, "./Music/Effects/grass_step_04.ogg"),
+			Gosu::Sample.new(@windows, "./Music/Effects/grass_step_05.ogg"),
+			Gosu::Sample.new(@windows, "./Music/Effects/projectile_block_01.ogg"),
+			Gosu::Sample.new(@windows, "./Music/Effects/projectile_block_02.ogg"),
+			Gosu::Sample.new(@windows, "./Music/Effects/projectile_block_03.ogg"),
+			Gosu::Sample.new(@windows, "./Music/Effects/sword_swing_01.ogg"),
+			Gosu::Sample.new(@windows, "./Music/Effects/sword_swing_02.ogg"),
+			Gosu::Sample.new(@windows, "./Music/Effects/sword_swing_03.ogg"),
+			Gosu::Sample.new(@windows, "./Music/Effects/sword_swing_04.ogg"),
+			Gosu::Sample.new(@windows, "./Music/Effects/sword_swing_05.ogg"),
+			Gosu::Sample.new(@windows, "./Music/Effects/sword_swing_06.ogg"),
+			Gosu::Sample.new(@windows, "./Music/Effects/sword_swing_07.ogg"),
+			Gosu::Sample.new(@windows, "./Music/Effects/sword_swing_08.ogg")
+		]
+			
+		
+		
 		@music[1].play 1, 1, true
+		# volume (0-1), speed, loop
+		# @music play_pan(pan = 0, vol = 1, speed = 1, looping = false) 
 	end
 end
